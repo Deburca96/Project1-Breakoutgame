52b758b1557428a77ed67c34e6de7e152e2820c7
Deburca96 <36736974+Deburca96@users.noreply.github.com>
2018-03-15 15:14:27 +0000

    Ball changes color on bounce

--- js/breakout.js
+++ js/breakout.js
@@ -5,26 +5,44 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
-var ballColour = "#0095DD"
+
 
 function drawBall() {
 	ctx.beginPath();
-	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+	ctx.arc(x,y, ballRadius, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+	
+}
+
+
+function drawBall() {
+	ctx.beginPath();
+	ctx.arc(x,y,ballRadius,0,Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
 }
-function draw () {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+function draw() {
+	ctx.clearRect(0,0,480,320);
 	drawBall();
 	x += dx;
 	y += dy;
-	if(y+ dy > canvas.height-ballRadius || y+dy <ballRadius){
-		dy=-dy;
-	}
-	if(x+dx > canvas.width-ballRadius || x+dx <ballRadius){
-		dx=-dx;
+	
+	//making change colour
+	if(y + dy >= 320 || y+dy <= 0) {
+		dy = -dy;
+		ctx.fillStyle ="pink";
+		ctx.fill();
 	}
 	
+	if(x + dx == 470 || x+dx <= ballRadius) {
+		dx = -dx;
+		ctx.fillStyle = "pink";
+		ctx.fill();
+	}
 }
-setInterval(draw, 10);
+
+setInterval(draw,50);
